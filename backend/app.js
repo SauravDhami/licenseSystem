e022dcf2d222200dd39e1178bfb4bb5a3964e848
SauravDhami <saugatdhami88@gmail.com>
2021-05-03 19:48:19 +0545

    Models separated and gitignore used

--- backend/app.js
+++ backend/app.js
@@ -4,52 +4,24 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose =  require('mongoose');
 
+const api = process.env.API_URL;
+
+
+const applicantsRouter = require('./routers/applicants');
+
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-const applicantSchema = mongoose.Schema({
-    name : String,
-    image : String,
-    countInStock : Number
-
-})
-
-const Applicant = mongoose.model('Applicant',applicantSchema);
+//routers
+app.use(`${api}/applicants`,applicantsRouter)
 
 require('dotenv/config');
 
 
-const api = process.env.API_URL;
 
-app.get(`${api}/products`,  (req,res)=>{
-    const applicant = {
-        id: 1,
-        name: "barber",
-        image: "url",
-    }
-    
-    res.send(applicant);
-})
 
-app.post(`${api}/products`,  (req,res)=>{
-    const applicant = new Applicant({
-       name: req.body.name,
-       image: req.body.image,
-       countInStock: req.body.countInStock
 
-    })
-
-    applicant.save().then((createdApplicant => {
-        res.status(201).json(createdApplicant)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-
-})
 
 
 mongoose.connect(process.env.CONNECTION_STRING, {
